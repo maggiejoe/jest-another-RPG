17cb36400221f8a7354897b4cc511fbe7948e64e
maggiejoe <maggiejoseph94@gmail.com>
2022-04-21 15:29:25 -0400

    enemy object created

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -2,7 +2,6 @@ const Potion = require('../lib/Potion');
 jest.mock('../lib/Potion.js');
 
 const Player = require ('../lib/Player');
-// jest.mock('../lib/Potion');
 
 console.log(new Potion());
 
@@ -88,7 +87,7 @@ test ("uses a potion from inventory", () => {
     const player = new Player('Dave');
     player.inventory = [new Potion(), new Potion(), new Potion];
     const oldCount = player.inventory.length;
-    
+
     player.usePotion(1);
 
     expect(player.inventory.length).toBeLessThan(oldCount);
